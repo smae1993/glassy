8ce2ef0d2c87de5a3469222250f7e42d427cd071
smae1993 <smae1993@gmail.com>
2022-07-16 23:20:03 +0430

    Build Package

--- lib/glassy_dialog.dart
+++ lib/glassy_dialog.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 class GlassyDialog extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Dialog();
   }
 }
